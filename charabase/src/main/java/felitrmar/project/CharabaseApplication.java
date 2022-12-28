7fca6e0f88c16f0ccdfc54b54ad2ef982143bd07
Trinidad <felipe.trinidad@solera.com>
2022-12-28 12:38:23 +0100

    Adding templates

---
+++ charabase/src/main/java/felitrmar/project/CharabaseApplication.java
@@ -0,0 +1,13 @@
+package felitrmar.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharabaseApplication.class, args);
+	}
+
+}
---
+++ charabase/src/test/java/felitrmar/project/CharabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package felitrmar.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
